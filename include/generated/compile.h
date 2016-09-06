263bd95772c9a6a5fb0eb0abe935e8259c34a806
root <root@localhost.localdomain>
2016-09-06 18:38:16 +0800

    sinlinx initial version

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 45 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#45 SMP PREEMPT Mon Sep 5 15:53:26 CST 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "localhost.localdomain"
+#define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (crosstool-NG linaro-1.13.1-2012.02-20120222 - Linaro GCC 2012.02) "
---
+++ modules/aw_schw/.tmp_versions/sunxi_schw.mod
@@ -0,0 +1,2 @@
+/root/work/sinlinx/a33/lichee/linux-3.4/modules/aw_schw/sunxi_schw.ko
+/root/work/sinlinx/a33/lichee/linux-3.4/modules/aw_schw/sunxi_schw_base.o /root/work/sinlinx/a33/lichee/linux-3.4/modules/aw_schw/libschw.a
